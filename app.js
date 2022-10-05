58516a395730639f99dd703e2bc13e179e899ef2
Julia Lipniewicz <julialipniewicz@yahoo.de>
2022-10-05 11:47:08 +0200

    first try

--- app.js
+++ app.js
@@ -13,13 +13,50 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+app.get('/views/beers.hbs', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      console.log(beers[0]);
+      res.render('beers', {
+        b: beers
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.render('page500');
+    });
+});
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(responseFromDB => {
+      res.render('beers', { beer: responseFromDB });
+    })
+    .catch(err => {
+      console.log(err);
+      res.render('page500');
+    });
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromRndBeer => {
+      res.render('random');
+    })
+    .catch(err => {
+      console.log(err);
+      res.render('page500');
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
